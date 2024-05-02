7a0649f9d5ab09dca2a71b6bb65336418a38a47b
RenanAmedeiros <renan.amedeiros@outlook.com>
2024-05-01 23:06:56 -0300

    Criado Primeiro Olá Mundo

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,3 +1,6 @@
 public class MinhaClasse {
-    
+    public static void main(String[] args) {
+        System.out.println("Olá , Obrigado a todos que acompanham primeiro programa em Java");
+        
+    }
 }
